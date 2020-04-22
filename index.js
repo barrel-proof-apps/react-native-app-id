cc16fa0d74880575392764277f477825de1dd176
Andrew Zuercher <az@barrelproofapps.com>
2020-04-22 10:29:14 -0400

    back to cli

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
+#!/usr/bin/env node
+
+const newID = process.argv[2];
+const newName = process.argv[3];
+const target = process.argv[4];
 const _ = require('lodash');
 const fs = require('fs');
 
-
-function transpose(newID, newName, target) {
-    if (newID && newID.split('.').length <2) {
-        console.log('Please type a valid bundle id e.g. com.solidstategroup.myapp');
-        return;
-    }
-
-    const promises = []
-
-    if (target == "android" || target == "both") {
-        promises.push(require('./src/android')(newID, newName))
-    }
-    if (target == "ios" || target == "both") {
-        promises.push(require('./src/ios')(newID, newName))
-    }
-
-
-
-    Promise.all(promises)
-        .then((items) => {
-
-            const ios = items[0];
-            const android = items[1];
-
-            _.each(ios, (data, location) => {
-                console.log(`Writing ${location}`);
-                fs.writeFileSync(location, data);
-            });
-
-            _.each(android, (data, location) => {
-                console.log(`Writing ${location}`);
-                fs.writeFileSync(location, data);
-            });
-
-        })
-        .catch(e => console.log(e));
+if (newID && newID.split('.').length <2) {
+    console.log('Please type a valid bundle id e.g. com.solidstategroup.myapp');
+    return;
 }
 
-module.exports.transpose = transpose
+const promises = []
+
+if (target == "android" || target == "both") {
+    promises.push(require('./src/android')(newID, newName))
+}
+if (target == "ios" || target == "both") {
+    promises.push(require('./src/ios')(newID, newName))
+}
+
+
+
+Promise.all(promises)
+    .then((items) => {
+
+        const ios = items[0];
+        const android = items[1];
+
+        _.each(ios, (data, location) => {
+            console.log(`Writing ${location}`);
+            fs.writeFileSync(location, data);
+        });
+
+        _.each(android, (data, location) => {
+            console.log(`Writing ${location}`);
+            fs.writeFileSync(location, data);
+        });
+
+    })
+    .catch(e => console.log(e));
